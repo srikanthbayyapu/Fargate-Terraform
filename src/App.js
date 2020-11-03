585833b60be098fccfdf5d3439939707a5cef9c0
Neikl <neiklohit62@gmail.com>
2020-11-03 21:14:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there
+          Hi there, Welcome to this app
         </p>
         <a
           className="App-link"
--- terraform/03-application/app.tf
+++ terraform/03-application/app.tf
@@ -117,7 +117,7 @@ resource "aws_appautoscaling_policy" "app_scaling" {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
 
-    target_value = 50
+    target_value = 10
   }
 
   depends_on = [aws_appautoscaling_target.nginx_fargate_target]
