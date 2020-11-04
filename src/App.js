2ece9e8f1781a11ef73e421eb6dbd3d9b0920953
Neikl <70577741+Neikl@users.noreply.github.com>
2020-11-04 13:32:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, Welcome1
+          Hi there, Welcome2
         </p>
         <a
           className="App-link"
